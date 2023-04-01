1e6f94a8258ae963a15750489169abdd58db7bfe
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-04-01 20:15:13 +0200

    WebSafer 1.0 - start

--- websafer/src/app/app.component.ts
+++ websafer/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'websafer';
+  faRight = faArrowRight;
+  faLeft = faArrowLeft;
 }
