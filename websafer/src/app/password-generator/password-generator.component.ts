cb5be7a459b65e3a19a65add00825a2e90773f95
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-04-08 17:23:34 +0200

    WebSafer 1.3.1 - passwd-generator form interacts

--- websafer/src/app/password-generator/password-generator.component.ts
+++ websafer/src/app/password-generator/password-generator.component.ts
@@ -8,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class PasswordGeneratorComponent implements OnInit {
 
   lenval: number = 8;
+  passwd: string = "";
+  AZ: boolean = true;
+  az: boolean = true;
+  num: boolean = true;
+  passwdstr: string = "Bad";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  checkpasswdstre()
+  {
+    if(this.passwd.length<7)
+    {
+      this.passwdstr = "Bad"
+    }
+    else if(this.passwd.length>7 && this.passwd.length<10)
+    {
+      this.passwdstr = "Good"
+    }
+    else
+    {
+      this.passwdstr = "Very good"
+    }
   }
 
   lenvalchange(event: any)
@@ -19,5 +40,30 @@ export class PasswordGeneratorComponent implements OnInit {
     this.lenval = event.target.value;
   }
 
+  passwdchange(event: any)
+  {
+    this.passwd = event.target.value;
+    this.checkpasswdstre();
+  }
+  AZchange()
+  {
+    this.AZ = !this.AZ;
+  }
+  azchange()
+  {
+    this.az = !this.az;
+  }
+  numchange()
+  {
+    this.num = !this.num;
+  }
 
+  generate()
+  {
+    console.log(this.lenval);
+    console.log(this.passwd);
+    console.log(this.AZ);
+    console.log(this.az);
+    console.log(this.num);
+  }
 }
